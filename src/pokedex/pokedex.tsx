44791d5707468a7b6a2a9f57243b66dae0cbdb8b
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-03-08 23:44:39 -0300

    refactor: Tipando; controlando o ciclo de vida usando o hook useffect; mostrando as habilidades do pokemon selecionado a partir da api

--- src/pokedex/pokedex.tsx
+++ src/pokedex/pokedex.tsx
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 interface PokedexProps {}
 
-const pokemonsArray: string[] = ['Pikachu', 'Ditto', 'Metapod', 'Magikarp'];
+interface PokemonListInterface {
+  name: string;
+  url: string;
+}
 
 export const Pokedex: React.FC<PokedexProps> = () => {
-  const [pokemons, setPokemons] = useState<string[]>(pokemonsArray);
-  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>('undefined');
+  const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<
+    PokemonListInterface | undefined
+  >(undefined);
+  const [selectedPokemonDetails, setSelectedPokemonDetails] = useState<
+    any | undefined
+  >(undefined);
+
+  useEffect(() => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(response => setPokemons(response.data.results));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedPokemon) return;
+
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`)
+      .then(response => setSelectedPokemonDetails(response.data));
+  }, [selectedPokemon]);
 
   return (
-    <div>
+    <>
       <h1>Pokedex</h1>
-
       Pokemons:
       {pokemons.map(pokemon => (
-        <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon}</button> 
+        <button onClick={() => setSelectedPokemon(pokemon)}>
+          {pokemon.name}
+        </button>
       ))}
-      <h2>Pokemon Selecionado:{selectedPokemon ||"Nenhum pokemon selecionado"}</h2>
-    </div>
+      <h2>
+        Pokemon Selecionado:
+        {selectedPokemon?.name || 'Nenhum pokemon selecionado'}
+      </h2>
+      {JSON.stringify(selectedPokemonDetails, undefined, 2)}
+    </>
   );
 };
